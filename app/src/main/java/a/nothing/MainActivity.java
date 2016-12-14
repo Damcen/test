7f57d2c7f49a2db2ffb203172102fc7c7b4833a4
Damcen <420908406@qq.com>
2016-12-14 10:01:00 +0800

    201612144

---
+++ app/src/main/java/a/nothing/MainActivity.java
@@ -0,0 +1,55 @@
+package a.nothing;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button btn1;
+    private Button btn2;
+    private FrameLayout fl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = ((Button) findViewById(R.id.btn1));
+        btn2 = ((Button) findViewById(R.id.btn2));
+        fl = ((FrameLayout) findViewById(R.id.fl));
+    }
+
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn1:
+                changeFragment(1);
+                break;
+
+            case R.id.btn2:
+                changeFragment(2);
+                break;
+        }
+    }
+
+    //改变整个Fragment
+    private void changeFragment(int flag)
+    {
+        FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (flag == 1)
+            transaction.replace(R.id.fl, new MyFragment());
+        else if (flag == 2)
+            transaction.replace(R.id.fl, new MyFragment2());
+        transaction.commit();
+    }
+
+}
